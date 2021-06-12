64124fea6548a8dec305246349760d7eed99e00c
Cleyton Brasil <cleytonb40@gmail.com>
2021-06-12 01:55:12 -0300

    set new types

--- dist/services/CreateUser.js
+++ dist/services/CreateUser.js
@@ -1,15 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// interface TechObject {
-//   title: string;
-//   experience: number;
-// }
-// interface CreateUserData {
-//   name?: string;
-//   email: string;
-//   password: string;
-//   techs: Array<string | TechObject>;
-// }
 function createUser(_a) {
     var name = _a.name, email = _a.email, password = _a.password, techs = _a.techs;
     var user = {
